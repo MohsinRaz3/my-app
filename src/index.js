418467b7090e979bdaef05d71b1b4f98fff293af
MohsinRaz3 <mohsinraz3@gmail.com>
2020-06-09 01:24:47 +0500

    Added hyperlink

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Hi(){
-  return <div className='firstcss'><h3> Hello from </h3> <strong> <h2>Mohsin Raz! </h2> </strong>
-
+  return <div className='firstcss'>  <h2>Tiny Wins Propel You Forward!</h2><br/> <h2>Hello from </h2> <strong> <a href="https://twitter.com/MohsinRaz_"> <h2>Mohsin Raz! </h2></a> </strong>
   <h3>Things I have learned about React so far:</h3>
+
   <center>
   <ul className="boom">
     <tr>Component</tr>
@@ -14,13 +14,14 @@ function Hi(){
     <tr>JSX</tr>
     <tr>Props</tr>
     <tr>ReactDOM</tr>
-  </ul> </center>
+  </ul> 
+  </center>
   
 
 <br/>
 <h4> Our Goal is to become a fullstack Webdeveloper to provide end to end solutions. </h4>
 <h3>Clock is ticking! {2+1} {1+1} {1}!</h3>
-<p>UPDATED: 08-06-2020</p>
+<p>UPDATE: 08-06-2020</p>
   </div>;
 }
 
