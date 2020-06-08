a0005a287c4e71430c2cb40469591b0e64c78d11
MohsinRaz3 <mohsinraz3@gmail.com>
2020-06-08 22:11:22 +0500

    updated content

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Hi(){
+  return <div className='firstcss'><h3> Hello from </h3> <strong> <h2>Mohsin Raz! </h2> </strong>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <h3>Things I have learned about React so far:</h3>
+  <center>
+  <ul className="boom">
+    <tr>Component</tr>
+    <tr>Render</tr>
+    <tr>Function</tr>
+    <tr>JSX</tr>
+    <tr>Props</tr>
+    <tr>ReactDOM</tr>
+  </ul> </center>
+  
+
+<br/>
+<h4> Our Goal is to become a fullstack Webdeveloper to provide end to end solutions. </h4>
+<h3>Clock is ticking! {2+1} {1+1} {1}!</h3>
+<p>UPDATED: 08-06-2020</p>
+  </div>;
+}
+
+ReactDOM.render(<Hi />, document.querySelector('#root'));
