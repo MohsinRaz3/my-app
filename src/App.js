81b9bdb699e77417530056019254de5d134f4a26
MohsinRaz3 <mohsinraz3@gmail.com>
2020-06-09 21:33:03 +0500

    Added props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <br/>
-          Updated by <h2> <a href="https://twitter.com/MohsinRaz_">Mohsin Raz</a></h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Me!
-        </a>
-      </header>
-    </div>
-  );
+function MyProgress(props){
+  return <div className='firstcss'>  
+  
+  <h2>Tiny Wins Propel You Forward!</h2><br/> <h2>Hello from  {props.name} </h2> <strong> <a href="https://twitter.com/MohsinRaz_"> <h2>  </h2></a> </strong>
+  <h3>Things I have learned about React so far:</h3>
+
+  <center>
+  <ul className="boom">
+    <tr></tr>
+    <tr>HTML/CSS/JavaScript</tr>
+    <tr>Node.js/npm/yarn</tr>
+    <tr>Deploying site on Surge using Github Actions</tr>
+    <tr>Functions/Component</tr>
+    <tr>ReactDOM/Render</tr>
+    <tr>JSX /Properties</tr>
+
+  </ul> 
+  </center>
+  
+
+<br/>
+<h4> Our Goal is to become a fullstack Webdeveloper to provide end to end solutions. </h4>
+<h3>Clock is ticking! {2+1} {1+1} {1}!</h3>
+<p>UPDATE: 09-06-2020</p>
+  </div>;
 }
-
-export default App;
+export default MyProgress;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import MyProgress from './App.js'
 
-function Hi(){
-  return <div className='firstcss'>  <h2>Tiny Wins Propel You Forward!</h2><br/> <h2>Hello from </h2> <strong> <a href="https://twitter.com/MohsinRaz_"> <h2>Mohsin Raz! </h2></a> </strong>
-  <h3>Things I have learned about React so far:</h3>
+ReactDOM.render(
 
-  <center>
-  <ul className="boom">
-    <tr>Component</tr>
-    <tr>Render</tr>
-    <tr>Function</tr>
-    <tr>JSX</tr>
-    <tr>Props</tr>
-    <tr>ReactDOM</tr>
-  </ul> 
-  </center>
-  
+<div> <h2><MyProgress name = 'Mohsin Raz' /></h2></div>,
+document.getElementById('root')
 
-<br/>
-<h4> Our Goal is to become a fullstack Webdeveloper to provide end to end solutions. </h4>
-<h3>Clock is ticking! {2+1} {1+1} {1}!</h3>
-<p>UPDATE: 08-06-2020</p>
-  </div>;
-}
-
-ReactDOM.render(<Hi />, document.querySelector('#root'));
+);
